a53a6ad2592441f90d1379a08ed5c0b91eb64107
bill <wcstrickland1@gmail.com>
2022-06-15 17:26:08 -0500

    Add project files.

---
+++ AdventureWorks.CustomerCarePortal.Web/Controllers/Customer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdventureWorks.CustomerCarePortal.Web.Controllers
+{
+    public class Customer : Controller
+    {
+
+
+        
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AdventureWorks.CustomerCarePortal.Web/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+
+
+// Get the container element
+var btnContainer = document.getElementById("customer-list-group");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("list-group-item");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
