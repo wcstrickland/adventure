1a2e2a0f8f7f8cd831c41bc319316ea6c30cb834
bill <wcstrickland1@gmail.com>
2022-06-16 09:27:35 -0500

    added highlight selected and remove customer button functionality

--- AdventureWorks.CustomerCarePortal.Web/wwwroot/js/site.js
+++ AdventureWorks.CustomerCarePortal.Web/wwwroot/js/site.js
@@ -7,16 +7,34 @@
 
 
 // Get the container element
-var btnContainer = document.getElementById("customer-list-group");
+let customerBtnContainer = document.getElementById("customer-list-group");
 
 // Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("list-group-item");
+let customerBtns = customerBtnContainer.getElementsByClassName("list-group-item");
 
 // Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+for (let i = 0; i < customerBtns.length; i++) {
+    customerBtns[i].addEventListener("click", function () {
+        let current = document.getElementsByClassName("active");
+        if (current.length === 0) {
+            this.className += " active";
+        }
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
 }
+
+
+
+let removeCustomerBtnContaier = document.getElementById("customer-list-group")
+let removeCustomerBtns = removeCustomerBtnContaier.getElementsByClassName("customer-list-closeButton")
+
+
+for (let i = 0; i < removeCustomerBtns.length; i++) {
+    removeCustomerBtns[i].addEventListener("click", () => {
+        //if (this.className.includes("active")){return}
+        let selected = document.getElementById(`list-group-item-${i - 1}`)
+        selected.remove()
+    });
+}
+
