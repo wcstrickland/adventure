fefa2b71c510b54b8274f3c7ea8e6b5e4dbd7eee
bill <wcstrickland1@gmail.com>
2022-06-17 13:06:54 -0500

    added comments to js funcs

--- AdventureWorks.CustomerCarePortal.Web/wwwroot/js/site.js
+++ AdventureWorks.CustomerCarePortal.Web/wwwroot/js/site.js
@@ -1,17 +1,8 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-
-
-
+﻿
 // Get the container element
 let customerBtnContainer = document.getElementById("customer-list-group");
-
 // Get all buttons with class="btn" inside the container
 let customerBtns = customerBtnContainer.getElementsByClassName("list-group-item");
-
 // Loop through the buttons and add the active class to the current/clicked button
 for (let i = 0; i < customerBtns.length; i++) {
     customerBtns[i].addEventListener("click", function () {
@@ -24,27 +15,26 @@ for (let i = 0; i < customerBtns.length; i++) {
     });
 }
 
-
-
+// grab buttons and add listener to remove instance from list on click
 let removeCustomerBtnContaier = document.getElementById("customer-list-group")
 let removeCustomerBtns = removeCustomerBtnContaier.getElementsByClassName("customer-list-closeButton")
-
-
 for (let i = 0; i < removeCustomerBtns.length; i++) {
     removeCustomerBtns[i].addEventListener("click", () => {
-        //if (this.className.includes("active")){return}
         let selected = document.getElementById(`list-group-item-${i - 1}`)
         selected.remove()
     });
 }
+
+// Set up datatable
 $(document).ready(function () {
     $('#example').DataTable({
         "lengthMenu":[25]
     });
 });
 
+// alert customer not found 
+//TODO: replace with MODAL
 let customerSearchButton = document.getElementById("search-customers-button")
-
 customerSearchButton.addEventListener("click", () => {
     alert("Customer Not Found")
 })
